ca2183344b4e713136e2ea5e72d784d465ab148c
keinn51 <soogood97@gmail.com>
2023-08-24 23:52:06 +0900

    이렇게 하면 되는군...?

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import Child from "./Child";
 function App() {
     return (
         <div className="App">
-            <Child elem={<h3>it is my child, not you</h3>} />
+            <Child
+                elem={
+                    <div id="find-child" style={{ backgroundColor: "orange" }}>
+                        <h1>it is my child, not you</h1>
+                        <h3>where is your child?</h3>
+                        <h4>chul soo~</h4>
+                        just joke lol lol
+                    </div>
+                }
+            />
         </div>
     );
 }
--- src/Child.js
+++ src/Child.js
@@ -1,3 +1,13 @@
+import { createElement } from "react";
+
 export default function Child({ elem }) {
-    return <>{elem}</>;
+    const Component = () =>
+        createElement(elem.type, { ...elem.props, style: { ...elem.props?.style, color: "red" } }, elem.props.children);
+
+    return (
+        <>
+            {/* {elem} */}
+            <Component />
+        </>
+    );
 }
